a0db8485bcabd6038fd0d033f1a088c0676b9ebd
IiliillIIllIIii <50038800+tatar0071@users.noreply.github.com>
2021-06-25 21:33:49 +0300

    Update Script.lua

---
+++ IonHub/Script.lua
@@ -0,0 +1,14 @@
+local a = game.GameId
+local b = {"595270616"}
+
+function c(d)
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/tatar0071/IonHub.rewrite/main/IonHub/"..d..".lua"))()
+end
+
+if a == b[1] then
+  c("SoA")
+elseif a == b[2] then
+  c("QuarantineZ")
+else
+  c("Universal")
+end
